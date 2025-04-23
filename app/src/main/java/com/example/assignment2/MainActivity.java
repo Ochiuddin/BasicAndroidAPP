4b12d0d9350c84ef58dcf4f8c1583aaf54ac0ced
Md Ochiuddin Miah <mdochiuddin.miah@ndsu.edu>
2025-04-23 15:02:03 -0500

    Assignment 6: Define, protect, and request permissions

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.assignment2;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
         explicitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(explicitIntent);
+                if (ContextCompat.checkSelfPermission(MainActivity.this, "com.example.assignment2.MSE412") == PackageManager.PERMISSION_GRANTED) {
+                    Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(explicitIntent);
+                }else{
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{"com.example.assignment2.MSE412"}, REQUEST_PERMISSION_CODE);
+                }
             }
         });
 
@@ -44,4 +54,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
